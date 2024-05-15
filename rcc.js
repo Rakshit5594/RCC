b0d93a377bc6cd741fde77e9d1df0d88c47db445
Rakshit5594 <111284464+Rakshit5594@users.noreply.github.com>
2024-05-16 00:43:09 +0530

    Add files via upload

---
+++ rcc.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    document.getElementById("rcc").innerHTML = "Welcome to RCC";
+  }, 5000);
+
+for (let i = 0; i <= 10; i++) {
+  setTimeout(function() {
+    document.getElementById("rcc").innerHTML = "Welcome to Rakshit crafts and creations";
+  }, 5000);
+}
